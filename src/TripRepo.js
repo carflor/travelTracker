3b34b8ba3cdf8f691df7e77026a7ace8aecf04ac
carflor <cfdeclet@gmail.com>
2020-06-03 23:00:52 -0600

    Sketch tripRepo class

---
+++ src/TripRepo.js
@@ -0,0 +1,11 @@
+class TripsRepo {
+  constructor(allTrips) {
+    this.allTrips = allTrips;
+  }
+
+  // add method to select trips that match the userID as an array of trips per user
+
+  // add method that re-defines the data as an obj with keys of the user id and all their trips per user as values within key
+}
+
+export default TripsRepo;
