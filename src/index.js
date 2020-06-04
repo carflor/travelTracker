493fed920fe1931d853d2604b3f5e464b1c5d46a
carflor <cfdeclet@gmail.com>
2020-06-04 15:56:57 -0600

    Add imports, html skeleton and partial files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,79 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
-import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
+// An example of how you tell webpack to use an image (also need to link to it in the index.html)
+// import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+import './css/base.scss';
+import $ from 'jquery';
+import moment from 'moment';
+// const today = moment().format("YYYY/MM/DD").split('-').join('/')
+
+
+// IMPORT ALL CLASSES BELOW
+import ApiFetch from './ApiFetch';
+import Traveler from './Traveler';
+import Trip from './Trip';
+import TripRepo from './TripRepo';
+import Agent from './Agent';
+import Destinations from './Destinations'
+// import domUpdates from './domUpdates'
+
+// Globals Variables
+let api = new ApiFetch();
+
+// ApiFetch
+const fetchApiData = () => {
+  const travelersData = api.getTravelers()
+  const tripsData = api.getTrips()
+  const destinationsData = api.getDestinations();
+  
+  Promise.all([travelersData, tripsData, destinationsData])
+    .then(dataSet => dataSet = {
+      travelersData: dataSet[0].travelers,
+      tripsData: dataSet[1].trips,
+      destinationsData: dataSet[2].destinations, 
+    }).then(dataSet => {
+      console.log(dataSet.travelersData)
+      console.log(dataSet.tripsData)
+      console.log(dataSet.destinationsData)
+      // send data to functions to instantiate! 
+    })
+    .catch(error => console.log(error.message))
+}
+
+
+
+
+
+
+
+
+
+
+// log in validation - translate to traveler 
+// $('.submit-button').click(() => {
+//   let username = $('.username-input')
+//   let password = $('.password-input')
+//   let customer = username.val().split('').splice(0, 8).join('')
+//   let customerId = Number(username.val().split('').splice(8).join(''))
+
+//   if ((customer === 'manager') && (password.val() === 'travel2020')) {
+//     displayManagerDashboard()
+//   } else if ((customer === 'traveler' && customerId) && (password.val() === 'travel2020')) {
+//     displayCustomerDashboard(customerId)
+//     loadCustomer(customerId, manager)
+//   } else {
+//     incorrectLogin()
+//   }
+// })
+
+
+// INCORRECT LOGIN - do this or download the npm for alerts
+// function incorrectLogin() {
+//   alert("Please Enter Correct Username and Password")
+//   $('.username-input').val('')
+//   $('.password-input').val('')
+// }
+
+fetchApiData()
