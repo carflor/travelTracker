3d0682968e388f61fb3eb9ec03e7c6ac770c15eb
carflor <cfdeclet@gmail.com>
2020-06-03 23:26:24 -0600

    Sketch of api fetch class

---
+++ src/ApiFetch.js
@@ -0,0 +1,24 @@
+class ApiFetch {
+  constructor() {
+    this.urlRoot = "https://fe-apps.herokuapp.com/api/v1/travel-tracker/data"
+  }
+
+  // getMethod for Single User (should this be used as opposed to all users w/ filter?)
+
+  // getMethod for users
+
+  // getMethod for trips
+
+  // getMethod for destinations
+
+  // postMethod for trip request
+
+  // postMethod for trip request APPROVAL
+  
+  // postMethod for trip request DENIAL
+
+  // deleteMethod for erasing trip history
+
+}
+
+export default ApiFetch;
