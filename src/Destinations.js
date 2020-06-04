57b46b064c7ec1b3b47816e756de58e5e27d9739
carflor <cfdeclet@gmail.com>
2020-06-03 23:09:42 -0600

    Add sketck of destinations class

---
+++ src/Destinations.js
@@ -0,0 +1,11 @@
+class Destinations {
+  constructor(allDestinations) {
+    this.destinations = allDestinations;
+  }
+
+  // method for grabbing a destination by ID 
+
+  // method for calculating cost for travel for this destination
+}
+
+export default Destinations; 
