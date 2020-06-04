bda1fd3b4f0ede12a18585f07e8c8a0c9010691c
carflor <cfdeclet@gmail.com>
2020-06-03 23:07:53 -0600

    Sketch traveler class with basic properties

---
+++ src/Traveler.js
@@ -0,0 +1,30 @@
+class Traveler {
+  constructor(user) {
+    this.id = user.id;
+    this.name = user.name;
+    this.travelerType = user.travelerType;
+
+    // create property for user trips (all)
+    // create property for user upcoming trips array
+    // create property for user past trips array
+    // create property for user pending trips array
+
+  }
+
+  // method for calculating pending trips
+  // method for calculating past trips
+  // method for calculating upcoming trips
+
+  // method to calculate amount spent on trips THIS YEAR
+
+  // method for trip request 
+}
+
+export default Traveler;
+
+
+{
+  "id": 1,
+  "name": "Ham Leadbeater",
+  "travelerType": "relaxer"
+  },
