d3f5fb941fdbafddf7f3a38299e9b40524bc3211
carflor <cfdeclet@gmail.com>
2020-06-05 12:26:04 -0600

    Fix describe block

--- test/destinations-test.js
+++ test/destinations-test.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import Destinations from '../src/Destinations';
 import destinationsTestData from './destinations-test-data.js';
 
-describe('Recipe', function() {
+describe('Destinations', function() {
   let destinationsInfo, allDestinations, destination;
 
   beforeEach(function() {
