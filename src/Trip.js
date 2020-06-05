95132bd13f1cae486464f36f9921164885e2b7ae
carflor <cfdeclet@gmail.com>
2020-06-04 21:15:57 -0600

    Removed white space

--- src/Trip.js
+++ src/Trip.js
@@ -1,16 +1,13 @@
 class Trip {
   constructor(tripObj) {
-    // this.id = randomize an id for the trip
     this.id = tripObj.id || Date.now();
     this.userID = tripObj.userID;
     this.destinationID = tripObj.destinationID;
     this.travelers = tripObj.travelers;
     this.date = tripObj.date;
     this.duration = tripObj.duration; 
-    // should pending be default value?
     this.status = tripObj.status || 'pending';
     this.suggestedActivities = tripObj.suggestedActivities;
-
   }
 }
 
