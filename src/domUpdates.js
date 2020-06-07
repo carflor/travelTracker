180850df7b725c4d62817c8c0f732c293d0f2b22
carflor <cfdeclet@gmail.com>
2020-06-07 17:06:32 -0600

    Add fn for form data transfers

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -17,7 +17,6 @@ class DomUpdates {
     $('.welcome-message').html(`${user.name.split(' ')[0]} Dashboard`)
     $('.travel-points').html(`Total Travel Points: ${user.getAmountSpentThisYear()}`)
     const pendingTrips = user.getPendingTrips()
-    console.log(pendingTrips, 'pending trips var')
     if (pendingTrips.length === 0) {
       $('.trips-box').html(`<p class='default-message'>No current travel plans<br> Please make a booking to accumulate Travel Points!<p>`)
     } else {
@@ -32,9 +31,9 @@ class DomUpdates {
             <section class="trip-card">
             <p class="destination">${trip.destination}</p>
             <p class="dates">${trip.date} to ${moment().add(trip.duration, 'day').format('YYYY/MM/DD')}</p>
-            <p class="day-amount">${trip.duration}</p>
+            <p class="day-amount">Duration: ${trip.duration} days</p>
             <section class="card-bottom">
-              <p class="group-size">Party Size:${trip.travelers}</p>
+              <p class="group-size">Party Size: ${trip.travelers}</p>
               <p class="status">${trip.status.toUpperCase()}</p>
             </section>
           </section>`
@@ -59,6 +58,50 @@ class DomUpdates {
       })
 
     }
+  }
+
+  displayTravelHistory() {
+    $('.travel-history-container').removeClass('hidden')
+    $('.new-trip-container').addClass('hidden')
+    $('.trips-box').html('');
+  }
+
+  displayDestinationsSearch() {
+    $('.travel-history-container').addClass('hidden')
+    $('.new-trip-container').removeClass('hidden')
+  }
+
+  displayBookingPage(event) {
+    let name = $(event.target).closest('.location-card').children().html()
+    $('.form-title').html(name)
+    $('.book-form').removeClass('hidden')
+    $('.traveler-details-box').addClass('blur')
+    $('.new-trip-container').addClass('blur')
+  }
+
+  displayConfirmationPage(event, allDestinations) {
+    // validate that all inputs are filled in
+    // validate that depart date is tomorrow at earliest
+    // fix issue with partySize where it does not auto get value of 1 placeholder!
+    event.preventDefault()
+    const locationName = $(event.target).closest('.process-booking').children().html()
+    const startDate = $(event.target).closest('.process-booking').find('#start-date').val()
+    const formatStartDate = moment(startDate).format("YYYY/MM/DD")
+    const endDate = $(event.target).closest('.process-booking').find('#return-date').val()
+    const formatEndDate = moment(endDate).format("YYYY/MM/DD")
+    const partySize = $(event.target).closest('.process-booking').find('#party-size').val()
+    const cityObj = allDestinations.destinations.find(city => city.destination === locationName) 
+    const beginDate = moment(startDate)
+    const finalDate = moment(endDate)
+    const dayAmount = finalDate.diff(beginDate, 'days', false)
+    const totalCost = allDestinations.getDestinationCost(cityObj, partySize, dayAmount)
+    $('.total-cost').html(`Approximate Total Cost: $${totalCost}`)
+    $('.confirm-location').html(`DESTINATION: ${locationName}`) 
+    $('.confirm-start-date').html(`DEPARTURE DATE: ${formatStartDate}`)
+    $('.confirm-return-date').html(`RETURN DATE: ${formatEndDate}`)
+    $('.confirm-party-size').html(`Number of Travelers: ${partySize}`)
+    $('.book-form').addClass('hidden')
+    $('.confirm-trip').removeClass('hidden')
   }
 
   incorrectLogin() {
